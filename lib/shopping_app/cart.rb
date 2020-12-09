d0bd7ee389be6402c6aa257cc377b299f6b00609
yodev <yodev2177@gmail.com>
2020-12-09 15:17:35 +0900

    Complete?

--- lib/shopping_app/cart.rb
+++ lib/shopping_app/cart.rb
@@ -22,6 +22,18 @@ class Cart
   end
 
   def check_out
+    # ## 要件
+    #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
+    self.owner.wallet.withdraw(self.total_amount)
+    @items.first.owner.wallet.deposit(self.total_amount)
+
+    #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
+    @items.map do |item|
+      item.owner = self.owner
+    end
+
+    #   - カートの中身（Cart#items）が空になること。
+    @items = []
   end
 
 end
